89a189acbfe2b6745c1d9c672af2a190c08212a5
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-05-30 21:09:35 +0000

    main file corrected

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -3,25 +3,77 @@
 Main file
 """
 
-filter_datum = __import__('filtered_logger').filter_datum
+import logging
+import mysql.connector
+import os
+import re
+from bcrypt import hashpw, gensalt
+from typing import List
 
-fields = ["password", "date_of_birth"]
-messages = [
-    ("name=egg;email=eggmin@eggsample.com;"
-     "password=eggcellent;date_of_birth=12/12/1986;"),
-    ("name=bob;email=bob@dylan.com;password=bobbycool;"
-     "date_of_birth=03/04/1993;")
-]
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
 
-get_db = __import__('filtered_logger').get_db
+def filter_datum(fields: List[str], redaction:
+                 str, message: str, separator: str) -> str:
+    return re.sub(
+        fr'(?<={separator}|^)({"|".join(fields)})=(.*?)(?={separator}|$)',
+        fr'\1={redaction}',
+        message
+    )
 
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
+
+
+def main():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+    for row in cursor.fetchall():
+        filtered_row = filter_datum(
+            PII_FIELDS, RedactingFormatter.REDACTION,
+            ";".join(map(str, row)), ";")
+        logger.info(f"Filtered data: {filtered_row}")
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
